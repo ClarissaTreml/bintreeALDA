81250a4cfab6827986b181e2dc0897611c5d283a
Treml <clarissa.treml@stud.fh-campuswien.ac.at>
2021-06-02 23:56:14 +0200

    left smaller right

--- bintree.cpp
+++ bintree.cpp
@@ -16,13 +16,76 @@ binTree *t_init(){
 }
 
 node *insertNode(node *start, int key, const char *data){
-    return NULL;
+    node * newNode;
+
+    if (start == NULL){
+        newNode = (node *) malloc(sizeof(node));
+        if (newNode != NULL){
+            newNode->data = (char *) data;
+            newNode->key = key;
+            newNode->right = newNode->left = NULL;
+        }
+        return newNode;
+    }
+
+    if (key < start->key){
+
+        newNode = insertNode(start->left, key, data);
+        if (start->left == NULL)
+        {
+            start->left = newNode;
+        }
+    }
+    else{
+
+        newNode = insertNode(start->right, key, data);
+        if (start->right == NULL){
+            start->right = newNode;
+        }
+    }
+    return newNode;
 }
 
 node *insert(binTree *t, int key, const char *data){
-    return NULL;
+    node * newNode;
+    newNode = insertNode(t->root, key, data);
+
+    if (t->root == NULL) t->root = newNode;
+    if (newNode != NULL) t->size++;
+
+    return newNode;
 }
 
 int evaluateExpression(node *start){
+
+
+    switch (start->data[0]) {
+
+        case '*':
+            return evaluateExpression(start->left) * evaluateExpression(start->right);
+
+        case '/':
+            return evaluateExpression(start->left) / evaluateExpression(start->right);
+
+        case '+':
+            if (start->left == NULL){
+                return evaluateExpression(start->right);
+            }
+            else return evaluateExpression(start->left) + evaluateExpression(start->right);
+
+        case '-':
+            if (start->left == NULL){
+                return evaluateExpression(start->right);
+            }
+            else if(start->left < start->right){
+                return evaluateExpression(start->right) - evaluateExpression(start->left);
+            }
+            else return evaluateExpression(start->left) - evaluateExpression(start->right);
+
+        default:return atoi(start->data);
+    }
+
+
+
     return 0;
 }
